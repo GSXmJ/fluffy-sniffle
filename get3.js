3d4686f738384f135031bf030227f57df2805bcb
GSXmJ <CbEECbGSXmJ@protonmail.com>
2022-02-03 11:24:02 +0000

    eslint, gitignore

--- get3.js
+++ get3.js
@@ -1,75 +1,73 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-const apiUrl = 'https://howrare.is/api/v0.1/collections';
+const apiUrl = 'https://howrare.is/api/v0.1/collections'
 
-function randomInt(max) { // random number from 0 to max 
-    return Math.floor(Math.random() * max)
-  };
+function randomInt (max) { // random number from 0 to max
+  return Math.floor(Math.random() * max)
+};
 
-async function fetchWithTimeout(resource, options = {}) { //timeout function because default timeout is like 300 seconds
-    const { timeout = 8000 } = options;
-    
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-    const response = await fetch(resource, {
-      ...options,
-      signal: controller.signal  
-    });
-    clearTimeout(id);
-    return response;
-  };
+async function fetchWithTimeout (resource, options = {}) { // timeout function because default timeout is like 300 seconds
+  const { timeout = 8000 } = options
 
-async function fetchJson(targetAddress, depth) {
-    try {
-      const response = await fetchWithTimeout(targetAddress, {
-        timeout: 6000
-      });
-      const getResponse = await response.json();
-      const responseData = getResponse.result.data;
-      let dataLength;
-      if (depth) {
-        dataLength = Object.keys(getResponse.result.data).length;
-      } else {
-        dataLength = Object.keys(getResponse.result.data.items).length;
-      }
-      return {responseData, dataLength};
-    } catch (error) {
-      // Timeouts if the request takes
-      // longer than 6 seconds
-      console.log(error.name === 'AbortError');
+  const controller = new AbortController()
+  const id = setTimeout(() => controller.abort(), timeout)
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal
+  })
+  clearTimeout(id)
+  return response
+};
+
+async function fetchJson (targetAddress, collection) {
+  try {
+    const response = await fetchWithTimeout(targetAddress, {
+      timeout: 6000
+    })
+    const getResponse = await response.json()
+    const responseData = getResponse.result.data
+    let dataLength
+    if (collection) {
+      dataLength = Object.keys(getResponse.result.data).length
+    } else {
+      dataLength = Object.keys(getResponse.result.data.items).length
     }
-  };
+    return { responseData, dataLength }
+  } catch (error) {
+    // Timeouts if the request takes
+    // longer than 6 seconds
+    console.log(error.name === 'AbortError')
+  }
+};
 
-async function main() {
-  var arg = process.argv.slice(2);
+async function main () {
+  const arg = process.argv.slice(2)
   console.log(arg)
-  let loopCount = parseInt(arg)
-  let list = []
+  const loopCount = parseInt(arg)
+  const list = []
 
-  let collections = await fetchJson(apiUrl, true)
+  const collections = await fetchJson(apiUrl, true)
 
   for (let i = 0; i < loopCount; i++) {
-  const randomCollection = randomInt(collections.dataLength)
+    const randomCollection = randomInt(collections.dataLength)
 
-  let collectionName = collections.responseData[randomCollection].name
-  let collectionUrl = collections.responseData[randomCollection].url
-  //console.log(apiUrl+collectionUrl)
-  
-  const jpegs = await fetchJson(apiUrl+collectionUrl, false)
+    const collectionName = collections.responseData[randomCollection].name
+    const collectionUrl = collections.responseData[randomCollection].url
+    // console.log(apiUrl+collectionUrl)
 
-  let randomJpeg = randomInt(jpegs.dataLength)
-  //console.log(randomJpeg)
-  let jpegUrl = jpegs.responseData.items[randomJpeg].image
-  //console.log(jpegUrl)
+    const jpegs = await fetchJson(apiUrl + collectionUrl, false)
 
-  let result = {collectionName, jpegUrl}
-  
-  list.push(result)
+    const randomJpeg = randomInt(jpegs.dataLength)
+    // console.log(randomJpeg)
+    const jpegUrl = jpegs.responseData.items[randomJpeg].image
+    // console.log(jpegUrl)
+
+    const result = { collectionName, jpegUrl }
+
+    list.push(result)
   }
 
   console.log(list)
 }
 
-main();
-
-
+main()
